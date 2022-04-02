212a770a291c4946f671283047b493dd7376e7c6
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-02 23:19:53 +0200

    fix(design) : routes ref #1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Index from "./pages/Index";
+import './assets/basic.scss'
+import UI from "./pages/UI";
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
+            <Route path="/_ui" element={<UI/>}/>
+            <Route path="/" element={<Index/>}></Route>
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
