73d28b9803c4e6c0526dd6e7d8a1541c15a8785c
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-02 23:12:51 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
