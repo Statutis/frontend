d13d69150098f228e5d1e598c8d77a895bce9fe8
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-05 00:05:45 +0200

    fix(design) : mise jour du title de la page blank ref #3

--- src/components/AppBase.jsx
+++ src/components/AppBase.jsx
@@ -3,16 +3,24 @@ import NavTop from "./NavTop";
 import HeaderLeft from "../img/base_left.svg";
 import HeaderRight from "../img/base_right.svg";
 import Footer from "./Footer";
+import {useEffect, useState} from "react";
 
 
 const AppBase = function (props) {
+
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        setTitle(document.title)
+    }, [document.title])
+
     return <>
         <NavTop/>
         <div className={"app-header"}>
             <img src={HeaderLeft} alt="Logo Gauche"/>
             <div>
                 <div>
-                    <h1>{document.title}</h1>
+                    <h1>{title}</h1>
                     <hr/>
                 </div>
             </div>
