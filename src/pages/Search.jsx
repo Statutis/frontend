c27e32e8124bf9519823c97e6a4556691417a88a
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-07 07:03:49 +0000

    feat : enregistrement de la page listant les groupes de services

---
+++ src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import useDocumentTitle from "../useDocumentTitle";
+
+const Search = function(){
+
+    useDocumentTitle("Liste des groupes de services")
+
+    return <div className="fluid-content">
+            
+    </div>
+}
+
+export default Search;
