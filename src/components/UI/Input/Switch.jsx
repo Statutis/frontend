329f2ffb1a4eeefa073686e19168c71d92846fb8
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-08 15:43:32 +0200

    fix(deisgn) : couleur de fond pour les switchs ref #15

--- src/components/UI/Input/Switch.jsx
+++ src/components/UI/Input/Switch.jsx
@@ -7,11 +7,11 @@ const Switch = function (props) {
 
     const [value, setValue] = useState("")
 
-    const updateValue = value => {
-        setValue(value.target.value)
-        console.log(value.target.value)
+    const updateValue = _ => {
+        const new_value = !value
+        setValue(new_value)
         if (props.hasOwnProperty("onChange"))
-            props.onChange(value.target.value)
+            props.onChange(new_value)
     }
 
     useEffect(() => {
@@ -19,7 +19,7 @@ const Switch = function (props) {
     }, [props.value])
 
     return <div className={"app-switch " + (props.className ?? "")}>
-        <input type="checkbox" value={value} onChange={updateValue} id={inputId}/>
+        <input type="checkbox" checked={value} onChange={updateValue} id={inputId}/>
         <label htmlFor={inputId}>
             <div className="switch"/>
             <span>{props.label ?? ""}</span>
