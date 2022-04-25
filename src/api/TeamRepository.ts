c69130c1f2eeecbdbe6a1d63db2571842d71c4fe
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-25 18:04:23 +0200

    feat : récupération de l'ID de l'équipe

--- src/api/TeamRepository.ts
+++ src/api/TeamRepository.ts
@@ -1,27 +1,23 @@
 import axios from "axios";
 import Team from "./Models/Team";
 
+const serialize = (data: Team): Team => {
+    const s = new Team()
+    s.id = data.id
+    s.name = data.name
+    s.color = data.color
+    s.ref = data.ref
+    s.groupRef = data.groupRef
+    s.userRef = data.userRef
+    return s;
+}
+
 export async function getTeams(): Promise<Team[]> {
     const response = await axios.get<Team[]>("/api/team")
-    return response.data.map(x => {
-        const s = new Team()
-        s.name = x.name
-        s.color = x.color
-        s.ref = x.ref
-        s.groupRef = x.groupRef
-        s.userRef = x.userRef
-        return s;
-    });
+    return response.data.map(serialize);
 }
 
 export async function getTeamsByRef(ref: string): Promise<Team> {
     const res = await axios.get<Team>(ref);
-
-    const s = new Team()
-    s.name = res.data.name
-    s.color = res.data.color
-    s.ref = res.data.ref
-    s.groupRef = res.data.groupRef
-    s.userRef = res.data.userRef
-    return s;
+    return serialize(res.data);
 }
