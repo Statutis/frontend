a29be8e9149a9e319b4ee4f2ca09c9128b102efa
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-26 20:46:12 +0200

    fix : design liste des équipes

--- src/pages/Team/TeamList.tsx
+++ src/pages/Team/TeamList.tsx
@@ -18,25 +18,27 @@ const TeamList = () => {
         getTeams().then(setTeams)
     }, [])
 
-    return <div className={"content"} id="team-list">
-        {teams.map(x => {
-            return <div className={"card"} key={x.ref}>
-                <div className={"card-content vstack stack-vcenter"}>
-                    <TeamAvatar team={x}/>
-                    <Link className="h3" to={"/teams/" + x.id}>{x.name}</Link>
-                    <div className="hstack stack-vcenter stack-center text-muted">
-                        <div className="hstack stack-vcenter" title="Nombre d'utilisateurs">
-                            <span className="material-icons mx-0">person</span>
-                            <span className="mx-0">{x.userRef.length}</span>
-                        </div>
-                        <div className="hstack stack-vcenter" title="Nombre de groupes">
-                            <span className="material-icons mx-0">style</span>
-                            <span className="mx-0">{x.groupRef.length}</span>
+    return <div className={"content"}>
+        <div id="team-list">
+            {teams.map(x => {
+                return <div className={"card"} key={x.ref}>
+                    <div className={"card-content vstack stack-vcenter"}>
+                        <TeamAvatar team={x}/>
+                        <Link className="h3" to={"/teams/" + x.id}>{x.name}</Link>
+                        <div className="hstack stack-vcenter stack-center text-muted">
+                            <div className="hstack stack-vcenter" title="Nombre d'utilisateurs">
+                                <span className="material-icons mx-0">person</span>
+                                <span className="mx-0">{x.userRef.length}</span>
+                            </div>
+                            <div className="hstack stack-vcenter" title="Nombre de groupes">
+                                <span className="material-icons mx-0">style</span>
+                                <span className="mx-0">{x.groupRef.length}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        })}
+            })}
+        </div>
         <div className="hstack stack-center mt-4">
             {user && <Link to={"/teams/add"} className={"btn"}>
                 <span className="material-icons">add</span>
